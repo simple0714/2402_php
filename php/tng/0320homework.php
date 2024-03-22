990d47d85a823e78c2c6de5fd5f593d35e954974
simple0714 <simple_0714@naver.com>
2024-03-22 17:43:03 +0900

    0322

--- php/tng/0320homework.php
+++ php/tng/0320homework.php
@@ -1,7 +1,14 @@
 <?php
 $end_num = 100;
-$arr_base = range(1,$end_num);
-foreach ($arr_base as $arr) {
-    if($arr % 3 != 0)
-    echo $arr."입니다.\n";
+$arr = range(1,$end_num);
+foreach ($arr as $val) {
+    if($val % 3 != 0)
+    echo $val."입니다.\n";
+}
+
+foreach($arr as $val){
+    if(($val % 3) === 0 ) {
+        continue;
+    }
+    echo $val."입니다.\n";
 }
